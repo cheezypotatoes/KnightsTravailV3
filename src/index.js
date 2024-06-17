49b4060cb66b437ebacfa541aade80bb9d70da8e
jorge <toxicpotato1223@gmail.com>
2024-06-17 17:34:06 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import { navigation } from "./create_nav";
-import me from "./img/literally me.jpg";
-
-
-navigation();
-
-document.body.appendChild(document.createElement("h1")).innerHTML = "Mr lack of idea restaurant page!";
-
-document.body.appendChild(Object.assign(document.createElement("img"), { src: me }));
-
-
-
-
-
+console.error('Hello World');
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,31 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const isProduction = process.env.NODE_ENV == 'production';
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
-const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
-
-
-
-const config = {
-    entry: {
-        main: './src/index.js',
-        login: './src/login.js',
-        about: './src/about.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'), // Use path.resolve to get an absolute path
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'Home.html',
-            chunks: ['main'], 
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'Login.html',
-            chunks: ['login'],
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'About.html',
-            chunks: ['about'],
-        }),
-    ],
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-         {
-           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-           type: 'asset/resource',
-         },
-        ],
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      chunks: ["main"],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-};
-
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        config.plugins.push(new MiniCssExtractPlugin());
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+    ],
+  },
 };
